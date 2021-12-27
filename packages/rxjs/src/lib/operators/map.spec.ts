a1d7f5f6e513e1dc5cf97e717266be683250979b
Alex Malkevich <malkevich.alex@gmail.com>
2021-12-27 13:59:55 +0100

    test(rxjs): simplify map unit tests to remove renderer

--- packages/rxjs/src/lib/operators/map.spec.ts
+++ packages/rxjs/src/lib/operators/map.spec.ts
@@ -1,27 +1,22 @@
 import {
-  BasicMarbleRuntime,
   MarbleSourceClosedEvent,
   MarbleSourceStartEvent,
   MarbleSourceValueEvent,
 } from '@rx-marbles/core';
 import { EmitableStubMarbleTimeline } from '@rx-marbles/core/testing';
-import { TextMarbleRenderer } from '@rx-marbles/renderer.text';
-import { StubTextHost } from '@rx-marbles/renderer.text/testing';
 import { MapMarbleOperator } from './map';
 
 describe('MapMarbleOperator', () => {
   it('should apply map function to input events', () => {
+    const callback = jest.fn();
     const input1 = new EmitableStubMarbleTimeline<string>();
     input1.getName.mockReturnValue('Letters');
     input1.getBounds.mockReturnValue({ start: 0, end: 10 });
     const mapCharCodeOperator = new MapMarbleOperator([input1], ([input1]) =>
       input1.charCodeAt(0),
     );
-    const mapRuntime = new BasicMarbleRuntime(mapCharCodeOperator);
-    const host = new StubTextHost();
-    const renderer = new TextMarbleRenderer({ host });
 
-    renderer.render(mapRuntime);
+    const sub = mapCharCodeOperator.subscribe(callback);
 
     input1.emit(new MarbleSourceStartEvent(0));
 
@@ -31,11 +26,10 @@ describe('MapMarbleOperator', () => {
 
     input1.emit(new MarbleSourceClosedEvent(10));
 
-    // Letters: (a)-(b)-(c)-X
-    expect(host.getBuffer()).toMatch(/\(a\)-+\(b\)-+\(c\)-+X/);
-    // Char Codes: (97)-(98)-(99)-X
-    expect(host.getBuffer()).toMatch(/\(97\)-+\(98\)-+\(99\)-+X/);
+    expect(callback).toHaveBeenCalledWith(new MarbleSourceValueEvent(0, 97));
+    expect(callback).toHaveBeenCalledWith(new MarbleSourceValueEvent(5, 98));
+    expect(callback).toHaveBeenCalledWith(new MarbleSourceValueEvent(7, 99));
 
-    renderer.dispose();
+    sub.cancel();
   });
 });
